40f07ba982889d3db874ff5bfa99d6002a062a79
Alexandre Melo <xandymelo@outlook.com.br>
2022-02-25 15:42:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-st.write(""" Recomendação de Livros """)
+st.write(""" # Recomendação de Livros
+          Vamos ver nosso primeiro gráfico do projeto!""")
