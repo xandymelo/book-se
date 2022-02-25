b9b9069896bf4f280c8a0a184bb3a927218940a0
Alexandre Melo <xandymelo@outlook.com.br>
2022-02-25 17:34:23 -0300

    add data

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 st.write(""" # Recomendação de Livros
           Vamos ver nosso primeiro gráfico do projeto!""")
+dataframe = pd.read_csv("books.csv")
+notas = list(dataframe['num_pages'])
+st.dataframe(dataframe)
+fig, ax = plt.subplots()
+ax.hist(notas, bins=20)
+
+st.pyplot(fig)
+#st.write(ax)
+#st.write(fig)
