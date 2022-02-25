276d942b3e549569fb33010f9adc6a27eaaed746
Alexandre Melo <xandymelo@outlook.com.br>
2022-02-25 18:21:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import title
 import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -6,11 +7,16 @@ import numpy as np
 st.write(""" # Recomendação de Livros
           Vamos ver nosso primeiro gráfico do projeto!""")
 dataframe = pd.read_csv("books.csv")
-notas = list(dataframe['num_pages'])
+df_art = dataframe.query('categories == "Art"')
+num_pag = list(dataframe['num_pages'])
+st.write("Todos os livros")
 st.dataframe(dataframe)
+st.write("livros com categoria: Arte")
+st.dataframe(df_art)
+notas = list(df_art['average_rating'])
 fig, ax = plt.subplots()
-ax.hist(notas, bins=20)
-
+ax.hist(notas, bins=5)
+ax.set_title('Avaliação dos livros de categoria Arte')
 st.pyplot(fig)
 #st.write(ax)
 #st.write(fig)
